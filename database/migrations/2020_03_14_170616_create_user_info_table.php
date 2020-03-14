560bae7082809e22be749ad2ef3b24138c11272e
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-15 01:43:26 +0800

    Added migrations

---
+++ database/migrations/2020_03_14_170616_create_user_info_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_info', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user')->notNull();
+            $table->string('status')->default('Your status');
+            $table->integer('age')->default(0);
+            $table->integer('weight')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_info');
+    }
+}
---
+++ database/migrations/2020_03_14_170855_create_user_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_settings', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('id_user')->notNull();
+            $table->integer('show_age')->length(1)->default(0);
+            $table->integer('show_weight')->length(1)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_settings');
+    }
+}
