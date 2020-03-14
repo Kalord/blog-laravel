41f1f1ae07c8bc6af01cffdddd1feedf88b5fe04
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-15 01:44:48 +0800

    Added UserSettings

---
+++ app/UserSettings.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserSettings extends Model
+{
+    /**
+     * Связанная с моделью таблица.
+     *
+     * @var string
+     */
+    protected $table = 'user_settings';
+}
