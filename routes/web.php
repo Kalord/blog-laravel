2a2623c0bc8f7dfe43135412162a7df38b19731e
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-14 18:23:15 +0800

    Added name for route

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
-});
+})->name('home');
 
 
-/** 
- * Login 
+/**
+ * Login
  */
 //Form
 Route::get('/login', 'UserController@login');
