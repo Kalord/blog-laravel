18606d65c4448342bd3ffdc95caee87600f3b02e
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-15 03:24:24 +0800

    Added logic for upload file

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,35 @@
-$('.avatar').mousemove((event) => {
-    console.log('Here');
+$('.avatar-update').click((event) => {
+    let file = $('.avatar-input').prop('files')[0];
+    let formData = new FormData();
+
+    formData.append('avatar', file);
+    formData.append('_token', $('[name="_token"').val());
+
+    console.log(formData);
+
+    $.ajax({
+        type: 'POST',
+        url: '/settings/avatar',
+        dataType: 'JSON',
+        cache: false,
+        contentType: false,
+        processData: false,
+        async: false,
+        data: formData,
+        success: (html) => {
+            $('.avatar-error-message').hide();
+            $('.avatar-success-message').fadeIn(300);
+            $('.avatar').attr('src', html.src);
+        },
+        error: (html) => {
+            html = JSON.parse(html.responseText);
+            $('.avatar-error-message').fadeIn(300);
+            $('.avatar-error-message').html(html.errors.avatar[0]);
+        }
+    });
 });
+
+$('.avatar-input').change((event) => {
+    let file = $('.avatar-input').prop('files')[0];
+    $('.avatar-label').html(file.name);
+})
