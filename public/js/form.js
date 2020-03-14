ddd1a5f9cdd5e23e9a235008dd6921e5dac32d9d
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-14 18:22:01 +0800

    Added logic for work with login form

--- public/js/form.js
+++ public/js/form.js
@@ -17,14 +17,45 @@ const submitHandler = (formClass, successCallback, errorCallback) => {
     });
 };
 
-submitHandler(
-    '.ajax-form',
-    (html) => {
-        removeShowErrors();
-    },
-    (html) => {
-        let errors = JSON.parse(html.responseText);
-        removeShowErrors();
-        showErrors(errors.errors)
-    }
-);
+if (window.location.pathname === '/registration') {
+    submitHandler(
+        '.ajax-form',
+        (html) => {
+            removeShowErrors();
+            $('#username').empty();
+            $('#username').html(html.name);
+            $('.success-message-reg').fadeIn(300);
+            setTimeout(() => {
+                window.location.href = '/login';
+            }, 3000);
+        },
+        (html) => {
+            let errors = JSON.parse(html.responseText);
+            removeShowErrors();
+            showErrors(errors.errors)
+        }
+    );
+}
+
+if (window.location.pathname === '/login') {
+    submitHandler(
+        '.ajax-form',
+        (html) => {
+            $('.error-message').hide();
+
+            if (html.status == 'error') {
+                $('.error-message').fadeIn(300);
+                $('.error-message').html(html.message);
+            }
+
+            if (html.status == 'success') {
+
+            }
+        },
+        (html) => {
+            let errors = JSON.parse(html.responseText);
+            removeShowErrors();
+            showErrors(errors.errors)
+        }
+    );
+}
