462d60e42f3af823ffa0029964a37a1ba93433ea
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-18 02:34:23 +0800

    Added migrations

---
+++ database/migrations/2020_03_17_092734_create_post_tag_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_post')->notNull();
+            $table->integer('id_tag')->notNull();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
---
+++ database/migrations/2020_03_17_093206_create_post_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->unique()->notNull();
+            $table->string('cover')->notNull();
+            $table->text('content')->notNull();
+            $table->integer('id_user')->notNull();
+            $table->integer('id_category')->notNull();
+            $table->integer('status')->default(0);
+            $table->integer('views')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
