1c69bb56b83e35ace253cdd14cf5179ee41e3c91
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-18 02:31:16 +0800

    Added EditorController

---
+++ app/Http/Controllers/EditorController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EditorController extends Controller
+{
+    public function index()
+    {
+        
+    }
+
+    public function create()
+    {
+        return view('create');
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function read()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+}
