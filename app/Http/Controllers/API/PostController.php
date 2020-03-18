6f4e9082cfc72a1b2fb3dac4f407b3760675d82e
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-19 00:19:35 +0800

    Added PostController

---
+++ app/Http/Controllers/API/PostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Post;
+
+class PostController extends Controller
+{
+    public function index(Request $request)
+    {
+        $limit = $request->input('limit') ? $request->input('limit') : Post::DEFAULT_LIMIT;
+        $pivot = $request->input('pivot') ? $request->input('pivot') : null;
+
+        $posts = Post::findByOptions($limit, $pivot);
+        foreach($posts as &$post) $post->joinTables();
+
+        return $posts;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->joinTables();
+        return $post;
+    }
+}
