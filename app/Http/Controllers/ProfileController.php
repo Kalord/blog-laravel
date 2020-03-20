87a30682111c33e5f905ae2314fba09c22ba5405
Artem <artem.tyutnev.developer@gmail.com>
2020-03-20 22:16:22 +0800

    Added logic for work with posts

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,25 +5,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\UserSettings;
+use App\Post;
 
 class ProfileController extends Controller
 {
     public function index()
     {
         return view('profile', [
-            'user' => Auth()->user()
+            'user'  => Auth()->user(),
+            'posts' => Post::findPostByIdUser(Auth()->id())
         ]);
     }
 
     public function user($id)
     {
-        $user = User::find($id);
-
-        if(!$user) abort(404);
+        $user = User::findOrFail($id);
 
         return view('profile', [
-            'user' => $user,
-            'settings' => UserSettings::get($user->id)
+            'user'      => $user,
+            'settings'  => UserSettings::get($user->id),
+            'posts'     => Post::findPostByIdUser($user->id)
         ]);
     }
 
