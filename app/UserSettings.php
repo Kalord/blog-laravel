b16a3192d0a646005a87de10d83912a7f79c7db6
Artem <artem.tyutnev.developer@gmail.com>
2020-03-20 22:22:22 +0800

    Added method: uploadAvatar

--- app/UserSettings.php
+++ app/UserSettings.php
@@ -3,9 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
 
 class UserSettings extends Model
 {
+    const AVATAR_DIR = '/uploads/avatar/';
+
     /**
      * Связанная с моделью таблица.
      *
@@ -16,5 +20,15 @@ class UserSettings extends Model
     public static function get($id)
     {
         return self::where('id_user', $id)->first();
+    }
+
+    public static function uploadAvatar(UploadedFile $avatar)
+    {
+        $fileName = Str::random(32) . '.' . $avatar->extension();
+        $avatar->move(public_path(self::AVATAR_DIR), $fileName);
+
+        $user = Auth()->user();
+        $user->avatar = self::AVATAR_DIR . $fileName;
+        $user->save();
     }
 }
