f2175a4cd766e26c4c39779f254749601ed333cc
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-28 23:27:27 +0800

    Added logic for actions update, index

--- app/Http/Controllers/EditorController.php
+++ app/Http/Controllers/EditorController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Category;
 use App\Tag;
+use App\Post;
 
 class EditorController extends Controller
 {
     public function index()
     {
-
+        return view('editor', [
+            'user'   => Auth()->user(),
+            'allViews' => Post::countViewsByIdUser(Auth()->id())
+        ]);
     }
 
     public function create()
@@ -21,9 +25,13 @@ class EditorController extends Controller
         ]);
     }
 
-    public function update()
+    public function update($id)
     {
-
+        return view('update', [
+            'post' => Post::findOrFail($id),
+            'categories' => Category::all(),
+            'tags' => Tag::all()
+        ]);
     }
 
     public function read()
