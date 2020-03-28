66b89331d47070d1b65e98125a7555c40b3328d0
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-03-28 23:29:03 +0800

    Added logic for work with user token

--- public/js/api/posts.js
+++ public/js/api/posts.js
@@ -10,13 +10,15 @@ const GET = 'GET';
  * @param successCallback - обработчик успешных запросов
  * @param errorCallback - обработчик неудачных запросов
  * @param limit - лимит выборки постов
+ * @param user_token - токен автора статей
  * @param pivot - опорный идентификатор для выборки
  * @param id_category - категория для выборки
- */
-const getPostsRequest = (successCallback, errorCallback, limit = 10, pivot = null, id_category = null) => {
-    let requestContainer = {limit: limit};
-    if(pivot) requestContainer.pivot = pivot;
-    if(id_category) requestContainer.id_category = id_category;
+ **/
+const getPostsRequest = (successCallback, errorCallback, limit = 10, user_token = null, pivot = null, id_category = null) => {
+    let requestContainer = { limit: limit };
+    if (user_token) requestContainer.id_user = user_token;
+    if (pivot) requestContainer.pivot = pivot;
+    if (id_category) requestContainer.id_category = id_category;
 
     $.ajax({
         type: GET,
