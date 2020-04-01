f51b6c3b629018a937ba177513f497e39ed7ffef
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-04-02 03:53:25 +0800

    Added StarsPost

---
+++ app/StarsPost.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class StarsPost extends Model
+{
+    protected $table = 'star_post';
+
+    protected $fillable = [
+        'id_user', 'id_post', 'stars'
+    ];
+
+    public static function getStars($id_user, $id_post)
+    {
+        $result = self::where('id_user', '=', $id_user)->
+                        where('id_post', '=', $id_post)->
+                        first();
+
+        return $result ? $result->stars : 0;
+    }
+
+    public static function newRecord($id_user, $id_post, $stars)
+    {
+        return self::create([
+            'id_user' => $id_user,
+            'id_post' => $id_post,
+            'stars'   => $stars
+        ]);
+    }
+
+
+    public static function addStars($id_user, $id_post, $stars)
+    {
+        $record = self::where('id_user', '=', $id_user)->
+                        where('id_post', '=', $id_post)->
+                        first();
+
+        if(!$record) return self::newRecord($id_user, $id_post, $stars);
+
+        $record->stars = $stars;
+        return $record->save();
+    }
+}
